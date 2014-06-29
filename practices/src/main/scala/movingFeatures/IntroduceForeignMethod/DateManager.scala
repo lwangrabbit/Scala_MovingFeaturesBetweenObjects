c5f6887b481fa2eb8a908c8a1abb00850931dfa6
wanglipeng <wanglipeng.007@gmail.com>
2014-06-29 13:55:28 +0800

    complete IntroduceForeignMethod.

--- practices/src/main/scala/movingFeatures/IntroduceForeignMethod/DateManager.scala
+++ practices/src/main/scala/movingFeatures/IntroduceForeignMethod/DateManager.scala
@@ -4,7 +4,12 @@ import java.util.Calendar
 
 class DateManager(previousDate:Calendar) {
   def getDate(): Calendar = {
+    nextDay()
+  }
+
+  def nextDay():Calendar = {
     previousDate.add(Calendar.DAY_OF_MONTH, 1)
     previousDate
   }
 }
+
